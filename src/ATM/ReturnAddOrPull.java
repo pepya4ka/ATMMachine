52b131a357d41a289643cd6e888ccf504d8dbd65
pepya4ka <ndrozdov@gmail.com>
2019-06-07 23:03:47 +0500

    Добавил новый класс

---
+++ src/ATM/ReturnAddOrPull.java
@@ -0,0 +1,11 @@
+package ATM;
+
+public class ReturnAddOrPull {
+    public StringBuffer action;
+    public int addOrPull;
+
+    ReturnAddOrPull() {
+        action = new StringBuffer();
+        addOrPull = 0;
+    }
+}
